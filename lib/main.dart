c7d61157ad302c2e67670a53f28b81c094757581
ProgrammingWithFaizan <faizan.abbas020@gmail.com>
2023-07-30 23:01:32 +0500

    home added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fudy/pages/onboarding_page.dart';
 
 import 'themes/theme_mode.dart';
 
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
         themeMode: ThemeMode.system,
         theme: lightThemeData,
         darkTheme: darkThemeData,
+        home: OnboardingPage(),
       ),
       designSize: const Size(414, 896),
     );
